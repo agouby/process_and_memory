733dcc36b971f6736091d79ecfcdad1274cd0b7e
agouby <antoine.gouby@gmail.com>
2019-03-20 00:08:10 +0100

    did spinlock

--- get_pid_info/get_pid_info.c
+++ get_pid_info/get_pid_info.c
@@ -12,6 +12,7 @@ static void	get_children(struct task_struct *task_pid, int *children)
 	struct task_struct *child = NULL;
 	int i = 0;
 
+	memset(children, 0, CHILD_MAX);
 	list_for_each_entry(child, &task_pid->children, sibling)
 	{
 		if (i == CHILD_MAX)
@@ -36,29 +37,50 @@ SYSCALL_DEFINE2(get_pid_info, struct pid_info __user*, retinfo, int, pid)
 		return -ESRCH;
 	task_pid = get_pid_task(pid_struct, PIDTYPE_PID);
 	if (!task_pid)
-		return -EFAULT;
+		return -ESRCH;
 	tmp = kmalloc(sizeof(struct pid_info), GFP_KERNEL);
 	if (!tmp)
 		return -ENOMEM;
+	get_fs_root(task_pid->fs, &root);
+	get_fs_pwd(task_pid->fs, &pwd);
+	buf = kmalloc(PATH_MAX, GFP_KERNEL);
+	if (!buf)
+	{
+		kfree(tmp);
+		return -ENOMEM;
+	}
+	spin_lock(root.dentry->d_lock);
+	dtmp = dentry_path_raw(root.dentry, buf, PATH_MAX);
+	spin_unlock(root.dentry->d_lock);
+	if (!dtmp)
+		goto dentry_failed;
+	strcpy(tmp->root_path, dtmp);
+
+	spin_lock(pwd.dentry->d_lock);
+	dtmp = dentry_path_raw(pwd.dentry, buf, PATH_MAX);
+	spin_unlock(pwd.dentry->d_lock);
+	if (!dtmp)
+		goto dentry_failed;
+	strcpy(tmp->pwd_path, dtmp);
+	kfree(buf);
+	
 	tmp->pid = task_pid_vnr(task_pid);
 	tmp->parent_pid = task_pid_vnr(task_pid->real_parent);
 	tmp->state = task_pid->state;
 	tmp->age = task_pid->start_time;
 	tmp->stack = task_pid->stack;
-	get_fs_root(task_pid->fs, &root);
-	get_fs_pwd(task_pid->fs, &pwd);
-	buf = kmalloc(PATH_MAX, GFP_KERNEL);
-	if (!buf)
-		return -ENOMEM;
-	memset(tmp->children, 0, sizeof(tmp->children));
-	dtmp = dentry_path_raw(root.dentry, buf, PATH_MAX);
-	strcpy(tmp->root_path, dtmp);
-	dtmp = dentry_path_raw(pwd.dentry, buf, PATH_MAX);
-	strcpy(tmp->pwd_path, dtmp);
 	get_children(task_pid, tmp->children);
+
 	if (copy_to_user(retinfo, tmp, sizeof(struct pid_info)))
+	{
+		kfree(tmp);
 		return -EFAULT;
+	}
+	kfree(tmp);
+	return 0;
+
+dentry_failed:
 	kfree(buf);
 	kfree(tmp);
-	return (0);
+	return -EFAULT;
 }
